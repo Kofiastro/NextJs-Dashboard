4866b3e854b70ca5de2411aa02a5f3e9006a6803
Kofiastro <astrokofi@gmail.com>
2024-12-03 19:36:59 +0000

    Next Js dashbaord finished

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import { Metadata } from 'next'
 
+
+export const metadata: Metadata = {
+  title: 'customer',
+};
 export default function page() {
   return (
     <div>Customers Page</div>
